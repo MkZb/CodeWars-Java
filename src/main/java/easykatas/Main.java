9567171601fe430d7e29113c85c3260f6a3ccf48
Mykola Zubets <kolya.zubets@gmail.com>
2022-08-01 19:01:02 +0300

    an easy kata

--- src/main/java/easykatas/Main.java
+++ src/main/java/easykatas/Main.java
@@ -223,6 +223,11 @@ public class Main {
         return true;
     }
 
+    public static double findUniq(double[] arr) {
+        Arrays.sort(arr);
+        return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0];
+    }
+
     public static void main(String[] args) {
         System.out.println(isAValidMessage("0"));
     }
